7335e1dcab63d5e73fe10226523dd571ecc753fa
Vladimir Makarić <vladamakaric@gmail.com>
2016-01-03 18:03:45 +0100

    add contour extraction, without inverting inside

--- contourExtraction.py
+++ contourExtraction.py
@@ -12,29 +12,36 @@ def getUpperLeftFilledPixelCoords(binImg):
 
 # def getClosedPathsFromBinImage(binImg):
 
-# def extractShapeContourPathFromBinImg(binImg):
-#
-# 	startCoord = getUpperLeftFilledPixelCoords(binImg)
-# 	prevCoord = startCoord
-# 	currentCoord = startCoord + Vec2(0,1)
-#
-# 	# every path starts downward on the left edge of the first pixel
-# 	path = [startCoord]
-#
-# 	while currentCoord != startCoord
-# 		path.append(currentCoord)
-# 		moveDir = getNextPathDir(binImg, currentCoord, prevCoord)
-# 		prevCoord = currentCoord
-# 		currentCoord += moveDir
+def extractShapeContourPathFromBinImg(binImg):
 
+	startCoord = getUpperLeftFilledPixelCoords(binImg)
+	prevCoord = startCoord
+	currentCoord = startCoord + Vec2(0,1)
+
+	# every path starts downward on the left edge of the first pixel
+	path = [startCoord]
+
+	while currentCoord != startCoord:
+		path.append(currentCoord)
+		moveDir = getNextPathDir(binImg, currentCoord, prevCoord)
+		prevCoord = currentCoord
+		currentCoord += moveDir
+
+
+	print path
+	return path
 
 def getNormalizedNeighborhoodNextDir(neighborhood):
 	if neighborhood[0][0] == 0: return Vec2(-1, 0)
 	if neighborhood[0][1] == 0: return Vec2(0, 1)
 	return Vec2(1,0)
 
+#returns direction in matrix coordinates (reverse y axis)
 def getNextPathDir(binImg, currentCoord, prevCoord):
 	direction = currentCoord - prevCoord
+
+	#convert direction to math coordinates (matrix coords have y axis negative)
+	direction.y *= -1
 
 	neighborhood = get4PixelNeighborhood(binImg, currentCoord)
 
@@ -42,28 +49,36 @@ def getNextPathDir(binImg, currentCoord, prevCoord):
 	
 	normNextDir = getNormalizedNeighborhoodNextDir(neighborhood)
 
-	return denormalizeNextDirection(direction, normNextDir)
+	mathCoordsNextDir = denormalizeNextDirection(direction, normNextDir)
+
+	#convert direction to matrix coordinates
+	return Vec2(mathCoordsNextDir.x, -mathCoordsNextDir.y)
 
 def denormalizeNextDirection(direction, newDirectionNormalized):
 	yaxis = direction
 	xaxis = direction.perpCW()
 
+	return newDirectionNormalized.x*xaxis + newDirectionNormalized.y*yaxis
+
+#direction is in regular math coordinates (not reverse y axis matrix coordinates)
 def normalize4PixelNeighborhood(neighborhood, direction):
 	yaxis = direction
 	xaxis = direction.perpCW()
 
 	print yaxis, xaxis
-
-	neighborCoords = [Vec2(0,0), Vec2(1,0), Vec2(1,1), Vec2(0,1)]
-	neighborCornerCenterCoords = [Vec2(-1,1), Vec2(1,1), Vec2(1,-1), Vec2(-1,-1)]
+	
+	#coords in the right handed coordinate system where the direction is the y axis
+	normalizedNeighborCoords = [Vec2(0,0), Vec2(1,0), Vec2(1,1), Vec2(0,1)]
+	normNeighborCornerCenterCoords = [Vec2(-1,1), Vec2(1,1), Vec2(1,-1), Vec2(-1,-1)]
 
 	offset = Vec2(1,-1)
 
-	normalizedNeighborCoords = ( (offset + v.x*xaxis + v.y*yaxis)/2 for v in neighborCornerCenterCoords) 
+	#transforming normalized coords in to their corresponding neighborhood coords
+	neighborCoords = ( (offset + v.x*xaxis + v.y*yaxis)/2 for v in normNeighborCornerCenterCoords) 
 	normalizedNeighborhood = [[0,0], [0,0]]
 
-	for nc, nnc in zip(neighborCoords, normalizedNeighborCoords):
-		normalizedNeighborhood[nc.y][nc.x] = neighborhood[nnc.y][nnc.x]
+	for nnc, nc in zip(normalizedNeighborCoords, neighborCoords):
+		normalizedNeighborhood[nnc.y][nnc.x] = neighborhood[nc.y][nc.x]
 
 	return tuple(normalizedNeighborhood[0]), tuple(normalizedNeighborhood[1])
 
@@ -84,8 +99,3 @@ def get4PixelNeighborhood(binImg, coord):
 		neighborhood[relativeCoord.y][relativeCoord.x] = 1
 			
 	return tuple(neighborhood[0]), tuple(neighborhood[1])
-
-def getNextPathDir(binImg, currentCoord, prevCoord):
-	neighborhood = get4PixelNeighborhood(binImg, currentCoord)
-	direction = currentCoord - prevCoord
-
--- test/test_contourExtraction.py
+++ test/test_contourExtraction.py
@@ -23,3 +23,39 @@ def test_normalize_4_pixel_neighborhood():
 	neighborhood2 = ((0,0), (1,1))
 
 	assert normalize4PixelNeighborhood(neighborhood2, Vec2(-1,0)) == ((1,0), (1,0))
+
+def test_get_normalized_neighborhood_next_dir():
+	neighborhood1 = ((0,0), (1,0))
+
+	assert getNormalizedNeighborhoodNextDir(neighborhood1) == Vec2(-1,0)
+
+
+
+def test_denormalize_next_direction():
+	assert denormalizeNextDirection(Vec2(1,0), Vec2(0,1)) == Vec2(1,0)
+
+
+def test_next_path_dir():
+	binimg = [[ 0, 0, 1, 1],
+			  [ 0, 0, 1, 1],
+			  [ 0, 1, 1, 1],
+			  [ 0, 1, 1, 0]]
+
+	assert getNextPathDir(binimg, Vec2(2,1), Vec2(2,0)) == Vec2(0,1)
+	assert getNextPathDir(binimg, Vec2(2,2), Vec2(2,1)) == Vec2(-1,0)
+	assert getNextPathDir(binimg, Vec2(3,3), Vec2(3,4)) == Vec2(1,0)
+	assert getNextPathDir(binimg, Vec2(3,4), Vec2(2,4)) == Vec2(0,-1)
+
+def test_extract_shape_contour_from_binimg():
+	binimg1 = [[ 1, 0],
+			   [ 0, 0]]
+
+	binimg2 = [[ 0, 0, 0, 0],
+			   [ 0, 0, 0, 0],
+			   [ 0, 1, 1, 1],
+			   [ 0, 1, 1, 0]]
+
+	assert extractShapeContourPathFromBinImg(binimg1) == [Vec2(0,0), Vec2(0,1), Vec2(1,1), Vec2(1,0)]
+
+	assert extractShapeContourPathFromBinImg(binimg2) == [Vec2(1,2), Vec2(1,3), Vec2(1,4), Vec2(2,4),
+		Vec2(3,4), Vec2(3,3), Vec2(4,3), Vec2(4,2), Vec2(3,2), Vec2(2,2)]
